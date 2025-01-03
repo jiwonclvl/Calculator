84a04b2b813819a83b90e6cea002c9f264fad882
jiwonclvl <clvlajrrhtlvek@gmail.com>
2025-01-03 16:37:07 +0900

    feat : 삭제 기능 구현
    연산 결과가 저장되어 있는 리스트의 첫번째 값 삭제하는 기능을 구현하고
    삭제된 값과 해당 값이 삭제된 리스트 내의 값들이 출력되도록 하였다.

--- src/main/java/com/example/calculator/App.java
+++ src/main/java/com/example/calculator/App.java
@@ -8,10 +8,9 @@ public class App{
 
         int input1, input2;
         char operator;
-        String end;
+        String end = "";
         Scanner sc = new Scanner(System.in);
         calculator calculate = new calculator();
-        ArrayList<Integer> answer_list = new ArrayList<Integer>();
 
         do {
 //            try{
@@ -31,22 +30,30 @@ public class App{
             System.out.print("양의 정수 입력:");
             input2 = sc.nextInt();
 
+//           양의 정수가 입력되지 않은 경우
+            if(input1 < 0 || input2 <0){
+                System.out.println("\n양의 정수를 입력해주세요.\n");
+                continue;
+            }
+
             System.out.print("사칙연산 기호 입력:");
             operator = sc.next().charAt(0);
 
-            int result = calculate.calculate(input1, input2, operator);
-//            System.out.println(input1 + " " +operator + " " +input2+ " = " + result);
+            //  올바른 연산자가 아닌 경우
+            if(operator != '+' && operator != '-' && operator != '*' && operator != '/'){
+                System.out.println("\n올바르지 않은 연산자 입니다.\n");
+                continue;
+            }
+
+            double result = calculate.calculate(input1, input2, operator);
+
             System.out.print("더 계산하시겠습니까? (exit 입력시 연산 종료):");
-
-
             end = sc.next();
             System.out.println();
         } while (!end.equals("exit")) ;
 
-//        public void setAnswer_list(ArrayList<Integer> answer_list){
-//
-//        }
-
+        ArrayList<Double> list = calculate.getAnswer_list();
+        calculate.setAnswer_list(list);
     }
 }
 
--- src/main/java/com/example/calculator/calculator.java
+++ src/main/java/com/example/calculator/calculator.java
@@ -3,10 +3,11 @@ package com.example.calculator;
 import java.util.ArrayList;
 
 public class calculator {
-    int answer;
-//    private ArrayList<Integer> answer_list = new ArrayList<Integer>();
 
-    public int calculate(int input1, int input2,char operator){
+    private ArrayList<Double> answer_list = new ArrayList<>();
+    double answer;
+
+    public double calculate(int input1, int input2,char operator){
         switch (operator) {
             case '+':
                 answer = input1 + input2;
@@ -22,22 +23,37 @@ public class calculator {
                 break;
             case '/':
                 if (input2 != 0) {
-                    answer = input1 / input2;
+                    answer = (double) input1 / input2;
                     System.out.println(input1 + " " +operator + " " +input2+ " = " + answer);
+                    break;
+
                 } else {
                     System.out.println("분모에 0이 들어갈 수 없습니다.");
+                    break;
                 }
-                break;
             default:
-                System.out.println("올바르지 않은 연산자 입니다.");
                 break;
         }
-//        answer_list.add(answer);
+        answer_list.add(answer);
         return answer;
     }
 
-//    public ArrayList<Integer> getAnswer_list(){
-//            return answer_list;
-//    }
+    public ArrayList<Double> getAnswer_list(){
+            return answer_list;
+    }
+
+    public void setAnswer_list(ArrayList<Double> Delete_list){
+            removeResult(Delete_list);
+    }
+
+    public void removeResult(ArrayList<Double> list){
+
+        System.out.print("첫번째 연산 값 " +list.get(0) + "을 삭제하였습니다." + "\n연산 결과 리스트: [" );
+        list.remove(0);
+        for(Double i: list){
+            System.out.print(" " + i + " ");
+        }
+        System.out.print("]");
+    }
 
 }
