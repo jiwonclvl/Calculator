daa428625bb9505c450835636fd015da037f0eec
jiwonclvl <clvlajrrhtlvek@gmail.com>
2025-01-07 18:21:39 +0900

    Feat : "Enum 타입 추가"
    Enum 타입을 이용해 연산자 타입을 관리하기 위해서 Enum OperatorType을
    추가하였고 추상 메서드를 오버라이딩하여 각 연산자에 맞는 동작을
    수행하도록 하였다.

---
+++ src/main/java/com/example/calculator/calculator3/ArithmeticCalculator.java
@@ -0,0 +1,4 @@
+package com.example.calculator.calculator3;
+
+public class ArithmeticCalculator {
+}
---
+++ src/main/java/com/example/calculator/calculator3/OperatorType.java
@@ -0,0 +1,44 @@
+package com.example.calculator.calculator3;
+
+public enum OperatorType {
+    PLUS("+") {
+        @Override
+        public double apply(int input1, int input2) {
+            return input1 + input2;
+        }
+    },
+    MINUS("-") {
+        @Override
+        public double apply(int input1, int input2) {
+            return input1 - input2;
+        }
+    },
+    MULTI("*") {
+        @Override
+        public double apply(int input1, int input2) {
+            return input1 * input2;
+        }
+    },
+    DIVIDE("/") {
+        @Override
+        public double apply(int input1, int input2) {
+            return (double) input1 / input2;
+        }
+    };
+
+    //필드
+    private String symbol;
+
+    // 생성자
+    OperatorType(String symbol) {
+        this.symbol = symbol;
+    }
+
+    //추상 메서드 생성
+    public abstract double apply(int input1, int input2);
+
+    //외부로 symbol 넘기기
+    public String getSymbol() {
+        return symbol;
+    }
+}
