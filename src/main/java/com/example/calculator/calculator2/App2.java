224bb5d7b472794ba4e018d95c5febb7554ff429
jiwonclvl <clvlajrrhtlvek@gmail.com>
2025-01-08 21:07:37 +0900

    Fix : "정수 입력 예외처리 적용"
    정수 입력이 뜨고 문자열 입력시 발생하는 오류를 해결하기 위해 try -
    catch문을 적용하였다.

--- src/main/java/com/example/calculator/calculator2/App2.java
+++ src/main/java/com/example/calculator/calculator2/App2.java
@@ -1,4 +1,5 @@
 package com.example.calculator.calculator2;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -8,34 +9,41 @@ public class App2 {
         Calculator calculate = new Calculator();
 
         do {
-            int firstInteger, secondInteger;
+            int firstNum, secondNum;
             String operators = "+ - * /";
-            String operator;
+            String inputOperator;
 
-            System.out.print("양의 정수 입력:");
-            firstInteger = sc.nextInt();
+            // 첫번째 정수 입력
+            try {
+                System.out.print("1번째 양의 정수 입력:");
+                firstNum = sc.nextInt();
 
-            System.out.print("양의 정수 입력:");
-            secondInteger = sc.nextInt();
+                System.out.print("2번째 양의 정수 입력:");
+                secondNum = sc.nextInt();
 
-            if (firstInteger < 0 || secondInteger < 0) {
-                System.out.println("\n양의 정수를 입력해주세요.\n");
+                if (firstNum < 0 || secondNum < 0) {
+                    System.out.println("\n양의 정수를 입력해주세요.\n");
+                    continue;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("\n정수만 입력해주세요.\n");
                 continue;
             }
 
             System.out.print("사칙연산 기호 입력:");
-            operator = sc.next();
+            inputOperator = sc.next();
 
-            if (!operators.contains(operator)) {
+            //
+            if (!operators.contains(inputOperator)) {
                 System.out.println("\n올바르지 않은 연산자 입니다.\n");
                 continue;
             }
-            if (secondInteger == 0 && "/".equals(operator)) {
+            if (secondNum == 0 && "/".equals(inputOperator)) {
                 System.out.println("분모에 0이 들어갈 수 없습니다.");
                 continue;
             }
 
-            calculate.calculate(firstInteger, secondInteger, operator);
+            calculate.calculate(firstNum, secondNum, inputOperator);
 
             System.out.print("더 계산하시겠습니까? (exit 입력시 연산 종료)\n");
             sc.nextLine();
@@ -44,6 +52,14 @@ public class App2 {
 
         List<Double> list = calculate.getAnswerlist();
         calculate.setAnswerlist(list);
+
+//        System.out.print("첫번째 값 삭제를 원한다면 y를 눌러주세요. y/n\n");
+//        if ("y".equals(sc.nextLine())) {
+//            calculate.setAnswerlist(list);
+//        } else {
+//            System.out.print("계산을 종료합니다.\n");
+//        }
+
     }
 }
 
