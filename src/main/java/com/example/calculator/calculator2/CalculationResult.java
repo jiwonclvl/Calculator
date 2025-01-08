154d7929ad7bc684df37d771d2eb201cce8071b0
jiwonclvl <clvlajrrhtlvek@gmail.com>
2025-01-08 14:19:42 +0900

    Feat : "연산 결과 출력 메서드 구현"
    기존에는 Calculator 클래스 내에서 연산과 출력을 모두 담당하였지만 연산
    결과를 출력하는 클래스를 만들어 한 클래스 내에서 하나의 동작만 수행할 수
    있도록 하였다.

---
+++ src/main/java/com/example/calculator/calculator2/CalculationResult.java
@@ -0,0 +1,8 @@
+package com.example.calculator.calculator2;
+
+public class CalculationResult {
+    //연산 결과 출력 메서드
+    public void calculationResult (int firstInteger, int secondInteger, char operator, double result) {
+        System.out.println(firstInteger + " " + operator + " " + secondInteger + " = " + result);
+    }
+}
